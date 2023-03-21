a6ec0161169141b8edde947217cf8e9c6b732b30
Mahipal Choudhary <mschoudhary81@gmail.com>
2023-03-21 17:06:25 +0530

    Template intigration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { LockScreenComponent } from './Components/lock-screen/lock-screen.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "lockscreen", component: LockScreenComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "**", redirectTo: "login", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { LockScreenComponent } from './Components/lock-screen/lock-screen.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { HeaderComponent } from './Shared/Components/header/header.component';
+import { FooterComponent } from './Shared/Components/footer/footer.component';
+import { SidebarComponent } from './Shared/Components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LockScreenComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
